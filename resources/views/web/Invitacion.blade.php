b3c6a7dabe54e922c571cbe56816f2436250b2cb
guilleguzman <guillermoguzman.2016@gmail.com>
2023-05-13 13:13:03 -0300

    Update Invitacion.blade.php

--- resources/views/web/Invitacion.blade.php
+++ resources/views/web/Invitacion.blade.php
@@ -4,6 +4,7 @@
 
 @section('head')
 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- font -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
