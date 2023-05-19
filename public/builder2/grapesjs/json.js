4429b30ca482a3a374a9427ab84a7c1137963154
guilleguzman <guillermoguzman.2016@gmail.com>
2023-05-19 15:38:17 -0300

    Update json.js

--- public/builder2/grapesjs/json.js
+++ public/builder2/grapesjs/json.js
@@ -2128,7 +2128,7 @@ var componentes = {
                       <div class="row text-xs-center" data-gjs-hoverable="false" data-gjs-layerable="false" data-gjs-editable="false" data-gjs-selectable="false" data-gjs-highlightable="false" data-gjs-droppable="false">
                         <div class="col-md-12 text-center m-0 p-0" data-gjs-hoverable="false" data-gjs-layerable="false" data-gjs-editable="false" data-gjs-selectable="false" data-gjs-highlightable="false" data-gjs-droppable="false">
                           <div class="carousel-wrap" data-gjs-hoverable="false" data-gjs-layerable="false" data-gjs-editable="false" data-gjs-selectable="false" data-gjs-highlightable="false" data-gjs-droppable="false">
-                            <div class="owl-carousel eventos" data-gjs-hoverable="false" data-gjs-layerable="false" data-gjs-editable="false" data-gjs-selectable="false" data-gjs-highlightable="false" data-gjs-droppable="false">
+                            <div class="owl-carousel eventos_owl" data-gjs-hoverable="false" data-gjs-layerable="false" data-gjs-editable="false" data-gjs-selectable="false" data-gjs-highlightable="false" data-gjs-droppable="false">
                   
                               <div class="item">
                                 <div class="card p-4" data-gjs-hoverable="false" data-gjs-layerable="false" data-gjs-editable="false" data-gjs-selectable="false" data-gjs-highlightable="false" data-gjs-droppable="false">
