d66baa9ff046ffcc464edf639f6d24c797b9a7d5
guilleguzman <guillermoguzman.2016@gmail.com>
2023-05-19 12:09:16 -0300

    Update dash.js

--- public/builder2/dash.js
+++ public/builder2/dash.js
@@ -4,6 +4,8 @@ $('.a1').click(function(event){
  
  $('.divTemplates').show();
  $('.divTemplates2').hide();
+ $('.divTemplates3').hide();
+ $('.divTemplates4').hide();
 });
 
 $('.a2').click(function(event){
@@ -12,4 +14,27 @@ $('.a2').click(function(event){
  
  $('.divTemplates').hide();
  $('.divTemplates2').show();
+ $('.divTemplates3').hide();
+ $('.divTemplates4').hide();
+});
+
+$('.a3').click(function(event){
+ 
+ event.preventDefault()
+ 
+ $('.divTemplates').hide();
+ $('.divTemplates2').hide();
+ $('.divTemplates3').show();
+ $('.divTemplates4').hide();
+    
+});
+
+$('.a4').click(function(event){
+ 
+ event.preventDefault()
+ 
+ $('.divTemplates').hide();
+ $('.divTemplates2').hide();
+ $('.divTemplates3').hide();
+ $('.divTemplates4').show();
 });
